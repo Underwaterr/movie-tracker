7bb6f6ab75fc45b87fd1bf0d75a829fe239bab29
Tyler Lane <sld.potato@gmail.com>
2018-11-25 19:29:15 -0600

    remove comment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
-//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
